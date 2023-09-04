de90fb6c216c079fe95ecc200e2a20887029bb0a
Octavio Gentile <142855781+OctavioGentile@users.noreply.github.com>
2023-09-04 19:03:15 -0300

    final

---
+++ ej7prac10.c
@@ -0,0 +1,59 @@
+//Octavio Gentile 44898506 C3
+//Ejercicio 7 practico 10
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct nodo {
+    char info[10];
+    struct nodo * next;
+    struct nodo * back;
+} TNodo;
+
+TNodo *p, *r, *s, *t;
+
+void MostrarLista(TNodo * list);
+
+int main () {
+
+    p = (TNodo * ) malloc (sizeof(TNodo));          //ultimo nodo por p
+    strcpy(p -> info, "estas?");
+    p -> next = NULL;
+    p -> back = NULL;
+
+    t = (TNodo * ) malloc (sizeof(TNodo));    
+    p -> back = t;
+    strcpy(t -> info, "como");
+    t -> next = p;
+
+    s = (TNodo * ) malloc (sizeof(TNodo));
+    t -> back = t;    
+    strcpy(s -> info, "Tito,");
+    s -> next = t;
+
+    r = (TNodo * ) malloc (sizeof(TNodo));          //primer nodo por r
+    s -> back = s;
+    strcpy(r -> info, "Hola");
+    r -> next = s;
+    r -> back = NULL;
+
+    MostrarLista(r);
+
+    return 0;
+}
+
+void MostrarLista (TNodo * list) {
+//lex local
+    TNodo *aux;
+//inicio
+    aux = list;
+    while(aux != NULL) {
+        printf("%s ",aux -> info);
+        aux = aux -> next;
+    }
+};
+
+//void MostrarLista (TNodo * list) {
+
+//};
