47aab9495f184fe09d971c26537ca47cbc4f7216
Octavio Gentile <142855781+OctavioGentile@users.noreply.github.com>
2023-10-02 18:16:58 -0300

    fin 1/4

---
+++ ej1a.c
@@ -0,0 +1,24 @@
+//Octavio Gentile 44898506 c3
+//SUMATORIA
+#include <stdio.h>
+#include <stlib.h>
+#include <string.h>
+
+int num;
+
+int SumatoriaAumento(int n);
+
+int main(){
+    printf("Ingrese un valor para ver la sumatoria desde 1 al numero dado, el numero debe
+ser natural");
+    scanf("%d", &num);
+    printf("Sumatoria con aumento: ", SumatoriaAumento(num));
+}
+
+int SumatoriaAumento(int n){
+    if (n==1){
+        return 1;
+    }else{
+        return n + SumatoriaAumento(n-1);
+    }
+}
