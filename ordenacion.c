aef76b74745dbf196d35e30df7354a69f37d5a97
Octavio Gentile <142855781+OctavioGentile@users.noreply.github.com>
2023-10-09 19:06:58 -0300

    final ord

--- ordenacion.c
+++ ordenacion.c
@@ -2,6 +2,7 @@
 //Algoritmo Burbuja
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #define Max 100
 
 typedef struct {
@@ -12,48 +13,47 @@ typedef struct {
 TData datos;
 int i;
 
-void InicializacionR (TData d);
-void Burbuja (TData d);
+void InicializacionR (TData *d);
+void Burbuja (TData *d);
 
 void main(){
+    srand (time(NULL));
     printf("ingrese la cantidad de elementos a cargar:");
-    scanf("%d", datos.cant);
-    InicializarR(datos);
+    scanf("%d", &datos.cant);
+    InicializacionR(&datos);
     i = 1;
     while (i <= datos.cant){
-        printf(datos.a[i]);
+        printf("%d\n", datos.a[i]);
         i++;
     }
-    Burbuja(datos);
-    for (i = 1, i <= datos.cant, i++){
-        printf(datos.a[i]);
+    Burbuja(&datos);
+    for (i = 1; i <= datos.cant; i++) {
+        printf("%d\n", datos.a[i]);
     }
 }
 
-void InicializacionR (TData d){
+void InicializacionR (TData *d){
    int i;
    i = 1;
-   while (i <= d.cant){
-        d.a[i] = random(1,100);
+   while (i <= d->cant){
+        d->a[i] = rand () %100 + 1;
         i++;
    } 
 }
 
-void Burbuja (Tdata d){
-    int i;
-    i = Max;
+void Burbuja (TData *d){
+    int i, j, temp;
+    i = d->cant;
     while (i>1){
         j = 1;
-        while(j<1){
-            if(d.a[j] > d.a[j+1]){
-                temp = d.a[j];
-                d.a[j] = d.a[j+1];
-                d.a[j+1] = temp;
+        while(j<i){
+            if(d->a[j] > d->a[j+1]){
+                temp = d->a[j];
+                d->a[j] = d->a[j+1];
+                d->a[j+1] = temp;
             }
             j++;
         }
         i--;
     }
 }
-
-
