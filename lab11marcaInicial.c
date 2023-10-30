528c5cb9b36134c41075b6545770091b32c00564
Octavio Gentile <142855781+OctavioGentile@users.noreply.github.com>
2023-10-30 19:17:50 -0300

    XD

--- lab11marcaInicial.c
+++ lab11marcaInicial.c
@@ -7,23 +7,22 @@ FILE *f;
 char elementoActual, elementoDado;
 int i, iguales, distintos;
 
-void CargarArchivo(FILE *(*g));
+void CargarArchivo(FILE *g);
 
 void main(){
-    CargarArchivo(&f);
+    CargarArchivo(f);
     f = fopen("secu.txt","r");
     printf("Ingrese el Caracter a buscar cuantos iguales y distintos hay: \n");
     scanf("%c", &elementoDado);
     getchar();
     iguales = 0;
     distintos = 0;
-    while(!feof(f)){
-        elementoActual = fgetc(f);
-       // printf("elemActual %c \n", elementoActual);
+    while((elementoActual = fgetc(f)) != EOF){
+        printf("\n elemActual %c \n", elementoActual);
         if(elementoActual == elementoDado){
-            iguales = iguales + 1;
+            iguales++;
         }else{
-            distintos = distintos + 1;
+            distintos++;
         }
     }
     fclose(f);
@@ -33,11 +32,11 @@ void main(){
     printf("%d", distintos);
 }
 
-void CargarArchivo(FILE *(*g)){
+void CargarArchivo(FILE *g){
     char msj [150];
     int cantidadElementos;
     char elem;
-    *g = fopen("secu.txt", "w");
+    g = fopen("secu.txt", "w");
     printf("Ingrese cuantos elementos desea cargar en el archivo: ");
     scanf("%d", &cantidadElementos);
     getchar();
@@ -45,7 +44,7 @@ void CargarArchivo(FILE *(*g)){
         printf("Ingrese un elemento: ");
         scanf("%c", &elem);
         getchar();
-        fputc(elem, *g);
+        fputc(elem, g);
     }
-    feof(*g);
+    feof(g);
 }
